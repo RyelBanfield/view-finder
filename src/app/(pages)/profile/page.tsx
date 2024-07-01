8887a0f5800a4f2a46247ec6c9b7fe44f7765871
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-01 18:32:45 -0400

    List albums on profile page

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
-import { fetchUserProfileAction } from "@/app/actions";
+import { fetchProfileAction } from "@/app/actions";
 import { Separator } from "@/components/ui/separator";
 import { createClient } from "@/utils/supabase/server";
 
@@ -10,7 +10,7 @@ import CreateAlbumForm from "./CreateAlbumForm";
 const ProfilePage = async () => {
   const supabase = createClient();
 
-  const userProfile = await fetchUserProfileAction();
+  const userProfile = await fetchProfileAction();
 
   if (!userProfile) redirect("/login");
   if (!userProfile.first_name || !userProfile.username) {
@@ -25,14 +25,14 @@ const ProfilePage = async () => {
   if (error) throw new Error(error.message);
 
   return (
-    <div className="flex flex-col gap-6 px-5 py-3">
+    <div className="flex flex-col gap-6 px-5 py-12 pb-36">
       <div className="flex justify-between">
         <div className="flex flex-col gap-1">
-          <h1 className="text-sm font-bold leading-none tracking-tighter">
+          <h1 className="font-bold leading-none tracking-tighter">
             {userProfile.first_name} {userProfile.last_name}
           </h1>
-          <p className="text-sm leading-none tracking-tighter text-muted-foreground">
-            @{userProfile.username}
+          <p className="leading-none tracking-tighter text-muted-foreground">
+            {userProfile.username}
           </p>
         </div>
 
@@ -41,22 +41,20 @@ const ProfilePage = async () => {
 
       <Separator />
 
-      <p className="text-sm font-bold leading-none tracking-tighter">
-        Your Albums
-      </p>
+      <p className="font-bold leading-none tracking-tighter">Your Albums</p>
 
-      <div className="grid min-h-80 grid-cols-2 gap-1">
+      <div className="grid min-h-80 grid-cols-2 gap-3">
         {albums.length === 0 ? (
-          <p className="col-span-2 tracking-tighter">
+          <p className="col-span-2 tracking-tight">
             Create your first album to get started.
           </p>
         ) : (
           albums.map((album) => (
-            <Link
-              key={album.id}
-              href={"/profile"}
-              className="h-60 w-full rounded-lg bg-black transition-transform duration-300 hover:scale-95"
-            ></Link>
+            <Link key={album.id} href={`album/${album.id}`}>
+              <div className="grid h-60 w-full place-items-center rounded-lg bg-primary px-3 py-2 text-secondary">
+                {album.name}
+              </div>
+            </Link>
           ))
         )}
       </div>
