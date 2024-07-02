35e831c2b8ca57dc68db4498705e853ce35277e0
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-01 22:40:53 -0400

    Create page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <div className="grid h-96 grow place-items-center">
+      <div>
+        <h1 className="text-center text-6xl font-bold">404</h1>
+        <p className="text-center text-2xl">Page not found</p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
