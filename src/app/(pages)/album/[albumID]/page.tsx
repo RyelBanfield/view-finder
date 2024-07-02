d8b3ae6e6fd53743681f936dbb2c71f34106b02a
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-02 18:15:47 -0400

    Style page and add empty upload button

--- src/app/(pages)/album/[albumID]/page.tsx
+++ src/app/(pages)/album/[albumID]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 
 import { fetchUserAuthAction } from "@/app/actions";
+import { Button } from "@/components/ui/button";
 
 import { fetchAlbumAction } from "./actions";
 import DeleteButton from "./DeleteButton";
@@ -14,12 +15,22 @@ const AlbumPage = async ({ params }: { params: { albumID: string } }) => {
   return (
     <div className="flex flex-col gap-6 px-5 py-12">
       <div className="flex justify-between">
-        <h1 className="text-4xl font-bold tracking-tighter">{album.name}</h1>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-4xl font-bold leading-none tracking-tighter">
+            {album.name}
+          </h1>
+
+          <p className="leading-none tracking-tighter text-muted-foreground">
+            {new Date(album.created_at).toDateString().slice(4)}
+          </p>
+        </div>
 
         {user && user.id === album.user_id && (
           <DeleteButton albumID={album.id} />
         )}
       </div>
+
+      <Button size={"sm"}>Upload Photos</Button>
     </div>
   );
 };
