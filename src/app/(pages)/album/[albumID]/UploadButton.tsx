7c58b92eb27261117487ee1b067bb0b22b3ffb9c
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-03 14:24:14 -0400

    Use compressor

--- src/app/(pages)/album/[albumID]/UploadButton.tsx
+++ src/app/(pages)/album/[albumID]/UploadButton.tsx
@@ -4,6 +4,7 @@ import "@uppy/core/dist/style.min.css";
 import "@uppy/dashboard/dist/style.min.css";
 
 import { Session } from "@supabase/supabase-js";
+import Compressor from "@uppy/compressor";
 import Uppy from "@uppy/core";
 import { Dashboard } from "@uppy/react";
 import Tus from "@uppy/tus";
@@ -21,7 +22,7 @@ import {
 } from "@/components/ui/sheet";
 import { createClient } from "@/utils/supabase/client";
 
-const UploadButton = () => {
+const UploadButton = ({ albumID }: { albumID: string }) => {
   const supabase = createClient();
 
   const [session, setSession] = useState<Session | null>(null);
@@ -32,65 +33,72 @@ const UploadButton = () => {
       .getSession()
       .then(({ data }) => data.session);
     getSession.then((session) => setSession(session));
-  }, [supabase.auth]);
+  }, []);
 
-  const uppy = new Uppy().use(Tus, {
-    endpoint: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/upload/resumable`,
-    retryDelays: [0, 3000, 5000, 10000, 20000],
-    headers: {
-      authorization: `Bearer ${session?.access_token}`,
-    },
-    uploadDataDuringCreation: true,
-    removeFingerprintOnSuccess: true,
-    chunkSize: 6 * 1024 * 1024,
-    allowedMetaFields: [
-      "bucketName",
-      "objectName",
-      "contentType",
-      "cacheControl",
-    ],
-  });
+  const uppy = new Uppy()
+    .use(Tus, {
+      endpoint: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/upload/resumable`,
+      retryDelays: [0, 3000, 5000, 10000, 20000],
+      headers: {
+        authorization: `Bearer ${session?.access_token}`,
+      },
+      uploadDataDuringCreation: true,
+      removeFingerprintOnSuccess: true,
+      chunkSize: 6 * 1024 * 1024,
+      allowedMetaFields: [
+        "bucketName",
+        "objectName",
+        "contentType",
+        "cacheControl",
+      ],
+    })
+    .use(Compressor);
 
   uppy.on("file-added", (file) => {
     file.meta = {
       ...file.meta,
       bucketName: "photos",
-      objectName: `${session!.user.id}/${uuidv4()}.${file.extension}`,
+      objectName: `${session!.user.id}/${albumID}/${uuidv4()}.${file.extension}`,
       contentType: file.type,
     };
   });
 
+  uppy.on("complete", (result) => {
+    console.log(
+      "Upload complete! We’ve uploaded these files:",
+      result.successful,
+    );
+  });
+
   return (
-    <>
-      <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
-        <SheetTrigger asChild>
-          <Button size={"sm"} className="w-full">
-            Upload Photos
-          </Button>
-        </SheetTrigger>
+    <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+      <SheetTrigger asChild>
+        <Button size={"sm"} className="w-full">
+          Upload Photos
+        </Button>
+      </SheetTrigger>
 
-        <SheetContent side={"bottom"}>
-          <SheetHeader>
-            <SheetTitle>Ready to upload?</SheetTitle>
-            <SheetDescription>
-              This will add photos to this public album.
-            </SheetDescription>
-          </SheetHeader>
+      <SheetContent side={"bottom"}>
+        <SheetHeader>
+          <SheetTitle>Ready to upload?</SheetTitle>
+          <SheetDescription>
+            This will add photos to this public album.
+          </SheetDescription>
+        </SheetHeader>
 
-          <br />
+        <br />
 
-          <Dashboard
-            uppy={uppy}
-            showProgressDetails={true}
-            waitForThumbnailsBeforeUpload
-            proudlyDisplayPoweredByUppy={false}
-            doneButtonHandler={async () => {
-              setSheetOpen(false);
-            }}
-          />
-        </SheetContent>
-      </Sheet>
-    </>
+        <Dashboard
+          uppy={uppy}
+          showProgressDetails={true}
+          waitForThumbnailsBeforeUpload
+          proudlyDisplayPoweredByUppy={false}
+          doneButtonHandler={async () => {
+            setSheetOpen(false);
+          }}
+        />
+      </SheetContent>
+    </Sheet>
   );
 };
 
