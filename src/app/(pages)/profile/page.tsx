eb9196bf654167bcb2d53f1c265648a96e5ff917
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-05 00:09:17 -0400

    Swap Link for TransitionLink

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 import {
@@ -6,6 +5,7 @@ import {
   fetchCurrentUserProfile,
   redirectIfMissingDetails,
 } from "@/app/actions";
+import TransitionLink from "@/components/TransitionLink";
 import { Button } from "@/components/ui/button";
 
 import CreateAlbumForm from "./CreateAlbumForm";
@@ -20,12 +20,12 @@ const ProfilePage = async () => {
   const albums = await fetchAlbumsByUserID(userProfile.id);
 
   return (
-    <div className="flex flex-col gap-6 px-5 py-6">
+    <div className="flex grow flex-col gap-6 p-6">
       <div className="flex justify-between">
         <div className="flex flex-col gap-1">
-          <h1 className="text-4xl font-bold leading-none tracking-tighter">
+          <h2 className="text-4xl font-bold leading-none tracking-tighter">
             {userProfile.first_name} {userProfile.last_name}
-          </h1>
+          </h2>
 
           <p className="leading-none tracking-tighter text-muted-foreground">
             {userProfile.username}
@@ -33,27 +33,28 @@ const ProfilePage = async () => {
         </div>
 
         <Button asChild size={"sm"} variant={"outline"} className="w-28">
-          <Link href="/profile/edit">Edit Profile</Link>
+          <TransitionLink href="/profile/edit">Edit Profile</TransitionLink>
         </Button>
       </div>
 
       <CreateAlbumForm />
 
-      {albums.length === 0 ? (
-        <div className="grid h-96 place-items-center">
+      {!albums || albums.length === 0 ? (
+        <div className="grid grow place-items-center">
           <p className="tracking-tight">
             Create your first album to get started.
           </p>
         </div>
       ) : (
         <div className="grid min-h-96 grid-cols-2 gap-3">
-          {albums.map((album) => (
-            <Link key={album.id} href={`album/${album.id}`}>
-              <div className="grid h-60 w-full place-items-center rounded-lg bg-primary px-3 py-2 text-secondary">
-                {album.name}
-              </div>
-            </Link>
-          ))}
+          {albums &&
+            albums.map((album) => (
+              <TransitionLink key={album.id} href={`album/${album.id}`}>
+                <div className="grid h-60 w-full place-items-center rounded-lg bg-primary px-3 py-2 text-secondary">
+                  {album.name}
+                </div>
+              </TransitionLink>
+            ))}
         </div>
       )}
     </div>
