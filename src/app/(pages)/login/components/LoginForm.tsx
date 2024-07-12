782adc8a0c9f45ad55def9eedf522cd5a93f8455
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-12 15:14:30 -0400

    Add button disabled state

--- src/app/(pages)/login/components/LoginForm.tsx
+++ src/app/(pages)/login/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { LoaderCircle } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -23,6 +24,7 @@ const formSchema = z.object({
 });
 
 const LoginForm = () => {
+  const [buttonDisabled, setButtonDisabled] = useState(false);
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -32,47 +34,64 @@ const LoginForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setButtonDisabled(true);
+    await loginWithEmail(values.email);
     setEmailSubmitted(true);
-    loginWithEmail(values.email);
   };
 
-  return !emailSubmitted ? (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="mx-auto flex max-w-sm grow flex-col justify-center gap-6 px-6 py-24"
-      >
-        <h1 className="text-center text-3xl font-bold">Login</h1>
+  return (
+    <>
+      {!emailSubmitted && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mx-auto flex grow flex-col justify-center gap-6 p-12"
+          >
+            <h1 className="text-center text-2xl font-bold">Login</h1>
 
-        <p className="text-center text-sm">
-          If you don&apos;t have an account, one will be created for you.
-        </p>
+            <p className="text-center text-muted-foreground">
+              If you don&apos;t have an account, one will be created for you.
+            </p>
 
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="name@mail.com" {...field} />
-              </FormControl>
-              <FormDescription>We will send you a magic link.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="name@mail.com" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    We will send you a magic link.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        <Button type="submit">Login</Button>
-      </form>
-    </Form>
-  ) : (
-    <div className="flex grow flex-col items-center justify-center gap-6 py-24">
-      <h1 className="text-center text-3xl font-bold">Magic Link Sent!</h1>
+            <Button size={"sm"} type="submit" disabled={buttonDisabled}>
+              {buttonDisabled && (
+                <LoaderCircle className="animate-spin h-4 w-4" />
+              )}
 
-      <p className="text-center text-sm">Check your email for a magic link.</p>
-    </div>
+              {!buttonDisabled && "Get magic link"}
+            </Button>
+          </form>
+        </Form>
+      )}
+
+      {emailSubmitted && (
+        <div className="flex grow flex-col justify-center gap-6 p-12">
+          <h1 className="text-center text-2xl font-bold">Magic Link Sent</h1>
+
+          <p className="text-center text-muted-foreground">
+            Check your email for a magic link.
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
