3989b034e5563046fe5895a01fdce580132ec715
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-12 15:14:15 -0400

    Remove without Relume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 import {
   fetchCurrentUserProfile,
   redirectIfMissingDetails,
 } from "./actions/userActions";
-import { AboutUs } from "./components/AboutUs";
-import { Hero } from "./components/Hero";
+import ImageSlider from "./components/ImageSlider";
 
 const Home = async () => {
   const userProfile = await fetchCurrentUserProfile();
@@ -12,8 +15,42 @@ const Home = async () => {
 
   return (
     <>
-      <Hero />
-      <AboutUs />
+      <section className="flex flex-col gap-6 px-6 py-12">
+        <div className="space-y-3">
+          <h1 className="text-balance text-center text-3xl font-bold">
+            Find yourself with View Finder
+          </h1>
+
+          <p className="text-balance text-center text-muted-foreground">
+            Discover and connect with amazing photographers.
+          </p>
+        </div>
+
+        <div className="flex justify-center space-x-3">
+          <Button size={"sm"} className="w-24" asChild>
+            <Link href="/explore">Explore</Link>
+          </Button>
+
+          <Button size={"sm"} variant={"secondary"} className="w-24" asChild>
+            <Link href="#about-us">Learn More</Link>
+          </Button>
+        </div>
+      </section>
+
+      <ImageSlider />
+
+      <section id="about-us" className="space-y-6 px-6 py-12">
+        <h2 className="text-balance text-2xl font-bold">
+          We&apos;re focused on experience.
+        </h2>
+
+        <p className="text-balance text-muted-foreground">
+          View Finder is a platform that connects photographers with their
+          clients. We&apos;re dedicated to delivering high-quality visuals and a
+          seamless user experience. We will be taking feedback and iterating on
+          our platform to ensure it meets the needs of our users.
+        </p>
+      </section>
     </>
   );
 };
