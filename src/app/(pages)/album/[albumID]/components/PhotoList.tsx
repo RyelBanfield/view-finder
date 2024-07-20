6f3229fcaf9a40ecfcd1560669c94083c6bb0cc2
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-20 00:32:13 -0400

    Create client side album and photo lists

---
+++ src/app/(pages)/album/[albumID]/components/PhotoList.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import TransitionLink from "@/components/TransitionLink";
+import { Tables } from "@/lib/database.types";
+
+const PhotoList = ({ photos }: { photos: Tables<"photos">[] }) => {
+  const containerVariants = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+      className="grid grow grid-cols-2 gap-4"
+    >
+      {photos.map((photo) => (
+        <motion.div
+          key={photo.id}
+          variants={itemVariants}
+          whileHover={{
+            opacity: 0.9,
+            rotateZ: -1,
+            transition: {
+              duration: 0.25,
+            },
+          }}
+          whileTap={{
+            scale: 0.95,
+            transition: {
+              duration: 0.25,
+            },
+          }}
+          className="relative aspect-square rounded shadow-2xl"
+        >
+          <TransitionLink href={`/photo/${photo.id}`}>
+            <Image
+              src={`http://127.0.0.1:54321/storage/v1/object/public/photos/${photo.file_path}`}
+              alt={photo.id}
+              fill
+              className="absolute rounded object-cover object-top"
+            />
+          </TransitionLink>
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+};
+
+export default PhotoList;
---
+++ src/app/(pages)/profile/components/AlbumList.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import { fetchRandomPhotoFromAlbum } from "@/app/actions/photoActions";
+import TransitionLink from "@/components/TransitionLink";
+import { Tables } from "@/lib/database.types";
+
+const AlbumList = ({ albums }: { albums: Tables<"albums">[] }) => {
+  const containerVariants = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
+  const [coverPhotos, setCoverPhotos] = useState<{
+    [key: string]: string | null;
+  }>({});
+
+  useEffect(() => {
+    const fetchCoverPhotos = async () => {
+      if (albums) {
+        const photos = await Promise.all(
+          albums.map(async (album) => {
+            const filePath = await fetchRandomPhotoFromAlbum(album.id);
+            return { albumId: album.id, url: filePath || null };
+          }),
+        );
+
+        const photoMap = photos.reduce(
+          (acc, photo) => {
+            if (photo.url) {
+              acc[photo.albumId] = photo.url;
+            }
+            return acc;
+          },
+          {} as { [key: string]: string | null },
+        );
+
+        setCoverPhotos(photoMap);
+      }
+    };
+
+    fetchCoverPhotos();
+  }, [albums]);
+
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+      className="grid grow grid-cols-2 gap-4"
+    >
+      {albums.map((album) => (
+        <motion.div
+          key={album.id}
+          variants={itemVariants}
+          whileHover={{
+            opacity: 0.9,
+            rotateZ: -1,
+            transition: {
+              duration: 0.25,
+            },
+          }}
+          whileTap={{
+            scale: 0.95,
+            transition: {
+              duration: 0.25,
+            },
+          }}
+          className="relative aspect-square rounded shadow-2xl"
+        >
+          <TransitionLink href={`album/${album.id}`}>
+            {coverPhotos[album.id] ? (
+              <>
+                <Image
+                  src={`http://127.0.0.1:54321/storage/v1/object/public/photos/${coverPhotos[album.id]}`}
+                  alt={album.name}
+                  fill
+                  className="absolute rounded object-cover object-top"
+                />
+                <div className="absolute flex h-full w-full items-end rounded bg-gradient-to-b from-transparent via-transparent to-black p-2">
+                  <p className="absolute text-sm text-white/60">{album.name}</p>
+                </div>
+              </>
+            ) : (
+              album.name
+            )}
+          </TransitionLink>
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+};
+
+export default AlbumList;
