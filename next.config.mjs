2f9ef509fac6ffbad535c5bb2c3adc1ae61135be
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-24 17:16:08 -0400

    Remove withP from Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import withPlaiceholder from "@plaiceholder/next";
+// import withPlaiceholder from "@plaiceholder/next";
 
 /** @type {import('next').NextConfig} */
 
@@ -15,4 +15,4 @@ const nextConfig = {
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
