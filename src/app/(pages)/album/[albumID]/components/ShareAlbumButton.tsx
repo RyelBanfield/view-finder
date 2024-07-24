0657be6d39d14e6052fdf1adf4c10481f3c6affe
Ryel Banfield <ryelbanfield@gmail.com>
2024-07-24 16:50:31 -0400

    Create new comp

---
+++ src/app/(pages)/album/[albumID]/components/ShareAlbumButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Share2Icon } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+
+const ShareAlbumButton = ({ baseURL }: { baseURL: string }) => {
+  const { toast } = useToast();
+  const pathname = usePathname();
+
+  const handleShare = async () => {
+    await navigator.clipboard.writeText(`${baseURL.slice(0, -1)}${pathname}`);
+
+    toast({
+      title: "Link copied",
+      description: "Share this album with your friends!",
+    });
+  };
+
+  return (
+    <Button size={"icon"} variant={"ghost"} onClick={handleShare}>
+      <Share2Icon className="size-4" />
+    </Button>
+  );
+};
+
+export default ShareAlbumButton;
